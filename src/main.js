da491516639659f02b633caa904a48babe9539b9
biglili <biglili@163.com>
2019-05-21 13:52:48 +0800

    富文本编辑器

--- src/main.js
+++ src/main.js
@@ -3,14 +3,14 @@ import App from './App'
 import router from './router'
 import iView from 'iview'
 import 'iview/dist/styles/iview.css'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
+// import axios from 'axios'
+// import VueAxios from 'vue-axios'
 
 
 Vue.config.productionTip = false
 
-Vue.use(iView);
-Vue.use(VueAxios,axios);
+// Vue.use(iView);
+// Vue.use(VueAxios,axios);
 
 /* eslint-disable no-new */
 new Vue({
